44991ac50e9ed6f8cc840a7b325f65d63ef742ca
Mathieu <117281622+Kawazam@users.noreply.github.com>
2023-12-20 17:31:30 +0100

    loop splines

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export const publicToken = "public_vbw4PInw41NCBjNW";
-export const mainSceneUUID = "dff6cf4b-0e4a-4ce8-aada-7c00f64a6ffe";
+export const mainSceneUUID = "29c5f51d-3c39-4efe-8cb9-cdec2d386ee7";
 export const characterControllerSceneUUID = "5b3cae52-19c9-4dba-859a-e599740cc9bf";
