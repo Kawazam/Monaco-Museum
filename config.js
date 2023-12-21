2b79777bde7648facdd4f9c0b7a4944334e9bfba
Mathieu <117281622+Kawazam@users.noreply.github.com>
2023-12-21 20:01:59 +0100

    merge-matthieu

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 export const publicToken = "public_vbw4PInw41NCBjNW";
 export const mainSceneUUID = "0ec3784e-cafa-4588-9232-40923d58b225";
 export const characterControllerSceneUUID = "5b3cae52-19c9-4dba-859a-e599740cc9bf";
+
+export const inventorySceneUUID = "2bc55c8a-6f6f-4535-9e58-605d52bac27c";
--- main.js
+++ main.js
@@ -3,10 +3,89 @@ import {
   publicToken,
   mainSceneUUID,
   characterControllerSceneUUID,
+  inventorySceneUUID,
 } from "./config.js";
+
+const inventoryJson = require("./inventory.json");
+
+var menu_display  = false;
+var inventory     = true;
+var stats         = false;
+var map           = false;
+
+var inventory_table = "";
+
+const button_inventory      = document.querySelector("#menu-menubar-title-inventory");
+const button_stats          = document.querySelector("#menu-menubar-title-stats");
+const button_map            = document.querySelector("#menu-menubar-title-map");
+
+const display_inventory     = document.querySelector("#menu-bloc-inventory");
+
+for(let i = 1; i < 7; i++) {
+  for(let j = 1; j < 5; j++) {
+    inventory_table += '<div class="menu-bloc-inventory-cell" id="inventory-cell'+i*j+'" style="left: '+((i-1)*15+i*1.4)+'%; top: '+((j-1)*20+j*4)+'%;"></div>';
+  }
+}
+
+display_inventory.innerHTML = inventory_table;
+
+document.addEventListener("keydown", checkKeyPress); //Toggle Inventory display
+
+//Toggle menu section
+button_inventory.addEventListener("click", function(){
+  inventory = true;
+  stats = map = false;
+  toggleMenuSection();
+});
+button_stats.addEventListener("click", function(){
+  stats = true;
+  inventory = map = false;
+  toggleMenuSection();
+});
+button_map.addEventListener("click", function(){
+  map = true;
+  inventory = stats = false;
+  toggleMenuSection();
+});
 
 //------------------------------------------------------------------------------
 window.addEventListener("load", InitApp);
+
+
+function checkKeyPress(event) {
+  const key = event.key;
+  if (key==='i') {
+    menu_display = !menu_display;
+  }
+
+  if (menu_display) {
+    document.querySelector("#menu").style.visibility = "visible";
+    document.querySelector("#menu-bloc-inventory").style.visibility = inventory ? "visible" : "hidden";
+    for (let element of document.querySelectorAll(".menu-bloc-inventory-cell")) element.style.visibility = inventory ? "visible" : "hidden";
+    document.querySelector("#menu-bloc-stats").style.visibility = stats ? "visible" : "hidden";
+    document.querySelector("#menu-bloc-map").style.visibility = map ? "visible" : "hidden";
+  } else {
+    document.querySelector("#menu").style.visibility = "hidden";
+    document.querySelector("#menu-bloc-inventory").style.visibility = "hidden";
+    for (let element of document.querySelectorAll(".menu-bloc-inventory-cell")) element.style.visibility = "hidden";
+    document.querySelector("#menu-bloc-stats").style.visibility = "hidden";
+    document.querySelector("#menu-bloc-map").style.visibility = "hidden";
+    document.querySelector("#display-canvas").focus();
+  }
+}
+
+
+function toggleMenuSection() {
+  button_inventory.classList.toggle("selected_title", inventory);
+  button_stats.classList.toggle("selected_title", stats);
+  button_map.classList.toggle("selected_title", map);
+
+  document.querySelector("#menu-bloc-inventory").style.visibility = inventory ? "visible" : "hidden";
+  for (let element of document.querySelectorAll(".menu-bloc-inventory-cell")) element.style.visibility = inventory ? "visible" : "hidden";
+  document.querySelector("#menu-bloc-stats").style.visibility = stats ? "visible" : "hidden";
+  document.querySelector("#menu-bloc-map").style.visibility = map ? "visible" : "hidden";
+}
+
 
 //------------------------------------------------------------------------------
 async function InitApp() {
