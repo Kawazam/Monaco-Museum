afc74d4ba74af9ce07729dd70c0a4597751b05d7
Mathieu <117281622+Kawazam@users.noreply.github.com>
2023-12-22 12:22:28 +0100

    config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 //Set ID of Publick Token
 export const publicToken = "public_vbw4PInw41NCBjNW";
 //Set ID of the Main Scene
-export const mainSceneUUID = "0ec3784e-cafa-4588-9232-40923d58b225";
+export const mainSceneUUID = "dff6cf4b-0e4a-4ce8-aada-7c00f64a6ffe";
 //Set ID of Character Controller Scene
 export const characterControllerSceneUUID = "5b3cae52-19c9-4dba-859a-e599740cc9bf";
 //Set ID of Inventory Scene
